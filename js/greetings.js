ab1ec1f74d4543949007a9c9ef4d97a610a78463
qvbranch <88976742+qvbranch@users.noreply.github.com>
2022-03-02 22:07:26 +0900

    Add files via upload

--- js/greetings.js
+++ js/greetings.js
@@ -1,30 +1,62 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+
+const loginButton = document.querySelector("#login-form button");
+const logoutButton = document.querySelector("#greetingBox button");
+
+const greetingBox = document.querySelector("#greetingBox");
+const greeting = document.querySelector("#greetingBox h1");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+//
+function onHidden(){
+    console.log(loginForm);
+    loginForm.classList.toggle(HIDDEN_CLASSNAME);
+    loginInput.classList.toggle(HIDDEN_CLASSNAME);
+    loginButton.classList.toggle(HIDDEN_CLASSNAME);
+
+    greetingBox.classList.toggle(HIDDEN_CLASSNAME);
+    greeting.classList.toggle(HIDDEN_CLASSNAME);
+    logoutButton.classList.toggle(HIDDEN_CLASSNAME);
+}
+
 function onLoginSubmit(event){
     event.preventDefault(); //기본동작 막기
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    loginInput.value = ""; //
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
+    //
+    onHidden();
 }
 
 function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
+//
+function onLogOut(){
+    localStorage.removeItem(USERNAME_KEY);
+    onHidden();
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+logoutButton.addEventListener("click", onLogOut);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null){
     // show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+    // loginForm.classList.remove(HIDDEN_CLASSNAME);
+    // loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     // show the greetings
     paintGreetings(savedUsername);
+    //
+    onHidden();
 }
